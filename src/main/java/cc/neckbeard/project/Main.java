8827ac8084e2a487b188fbfffa7dec42bd50b149
nothub <48992448+nothub@users.noreply.github.com>
2021-05-24 20:46:32 +0200

    add apache.commons.cli

--- src/main/java/cc/neckbeard/project/Main.java
+++ src/main/java/cc/neckbeard/project/Main.java
@@ -1,12 +1,42 @@
 package cc.neckbeard.project;
 
+import org.apache.commons.cli.*;
 import org.tinylog.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
-        Logger.debug("Starting");
-        Logger.info(args.length > 0 ? String.join(", ", args) : "💩");
+
+        Options options = new Options();
+        Option optionB = new Option("b", "bar", false, "bar example");
+        options.addOption(optionB);
+        Option optionA = new Option("f", "foo", true, "foo example (required)");
+        optionA.setRequired(true);
+        options.addOption(optionA);
+
+        CommandLine cmd;
+        try {
+            cmd = ((CommandLineParser) new DefaultParser()).parse(options, args);
+        } catch (ParseException e) {
+            Logger.error(e.getMessage());
+            new HelpFormatter().printHelp(
+                new java.io.File(
+                    Main
+                        .class
+                        .getProtectionDomain()
+                        .getCodeSource()
+                        .getLocation()
+                        .getPath()
+                ).getName(),
+                options);
+            System.exit(1);
+            return;
+        }
+
+        Logger.info("Hello World!");
+        Logger.debug("foo: " + cmd.getOptionValue("foo"));
+        Logger.debug("bar: " + Boolean.valueOf(cmd.hasOption("bar")).toString());
+
     }
 
 }
